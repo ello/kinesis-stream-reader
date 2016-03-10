19b0880b185373abfcd86ed079db23a01859c18c
Justin-Holmes <justin.ryan.holmes@icloud.com>
2016-03-10 14:48:44 -0700

    Fix issue where there are multiple schemas

--- lib/stream_reader/avro_parser.rb
+++ lib/stream_reader/avro_parser.rb
@@ -9,7 +9,7 @@ class AvroParser
     buffer = StringIO.new(@data)
     reader = Avro::DataFile::Reader.new(buffer, Avro::IO::DatumReader.new)
     reader.each do |record|
-      schema_name = reader.datum_reader.readers_schema.name
+      schema_name = reader.datum_reader.readers_schema.try(:name) || reader.datum_reader.readers_schema.schemas.last.name
       yield record, schema_name
     end
   end
