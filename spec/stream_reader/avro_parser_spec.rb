7d7691c3f7087d17e3e5107bea90b323651def3e
Justin Holmes <justin.ryan.holmes@icloud.com>
2017-05-04 12:13:50 -0600

    Include shard_id when processing events as well

--- spec/stream_reader/avro_parser_spec.rb
+++ spec/stream_reader/avro_parser_spec.rb
@@ -4,13 +4,15 @@ require_relative '../../lib/stream_reader/avro_parser'
 describe AvroParser do
   let(:data) { File.read(File.join(File.dirname(__FILE__), '..', 'support', 'user_was_created.avro')) }
   let(:seq_number) { 'asdf123' }
-  let(:parser) { described_class.new(data, seq_number) }
+  let(:shard_id) { '1' }
+  let(:parser) { described_class.new(data, seq_number, shard_id) }
 
   it 'parses an Avro message' do
     parser.each_with_schema_name do |record, opts|
       expect(opts[:schema_name]).to eq('UserWasCreated')
-      expect(opts[:seq_number]).to eq seq_number
+      expect(opts[:sequence_number]).to eq seq_number
       expect(opts[:raw_data]).to eq data
+      expect(opts[:shard_id]).to eq shard_id
       expect(record).to eq({
         "id"=>"1",
         "username"=>"hello",
